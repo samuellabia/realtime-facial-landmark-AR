8d1a238a431385e924992701c70eb4eb4f573924
jeldibia <samuella.jeldi-bia2@etu.univ-lorraine.fr>
2026-01-04 16:35:27 +0100

    ajout du flip pour un effet miroir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 # Initialisation Mediapipe Face Mesh
 
@@ -10,7 +11,6 @@ mp_face_mesh = mp.solutions.face_mesh  # usage du module de détection de visage
 face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1,refine_landmarks=True)  # création du détecteur pour un visage.
 
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1, color=(255,255,0))
-
 # Ouvre la webcam
 cap = cv2.VideoCapture(0) #0 pour caméra principale
 
@@ -36,9 +36,7 @@ while cap.isOpened():
             connections=mp_face_mesh.FACEMESH_RIGHT_EYEBROW,
             landmark_drawing_spec=None,
             connection_drawing_spec=drawing_spec)
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow("Webcam", frame)
-    
+    cv2.imshow("Webcam",cv2.flip(frame, 1))
     # Quitte avec ESC
     if cv2.waitKey(1) & 0xFF == 27:
         break
