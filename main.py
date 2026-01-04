0d2bdb5310c94c3f4599f1a8623f070fcece29f7
jeldibia <samuella.jeldi-bia2@etu.univ-lorraine.fr>
2026-01-04 15:31:48 +0100

    ajustement pour le dessin des sourcils uniquement

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh  # usage du module de détection de visage
 face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1,refine_landmarks=True)  # création du détecteur pour un visage.
 
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1, color=(255,255,0))
 
 # Ouvre la webcam
 cap = cv2.VideoCapture(0) #0 pour caméra principale
@@ -18,8 +18,7 @@ while cap.isOpened():
     success, frame = cap.read() #lecture de l'image en continu
     if not success:
         break
-
-
+    
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = face_mesh.process(rgb_frame)
     
@@ -28,24 +27,15 @@ while cap.isOpened():
         mp_drawing.draw_landmarks(
             image=frame,
             landmark_list=face_landmarks,
-            connections=mp_face_mesh.FACEMESH_TESSELATION,
+            connections=mp_face_mesh.FACEMESH_LEFT_EYEBROW,
             landmark_drawing_spec=None,
-            connection_drawing_spec=mp_drawing_styles
-            .get_default_face_mesh_tesselation_style())
+            connection_drawing_spec=drawing_spec)
         mp_drawing.draw_landmarks(
             image=frame,
             landmark_list=face_landmarks,
-            connections=mp_face_mesh.FACEMESH_CONTOURS,
+            connections=mp_face_mesh.FACEMESH_RIGHT_EYEBROW,
             landmark_drawing_spec=None,
-            connection_drawing_spec=mp_drawing_styles
-            .get_default_face_mesh_contours_style())
-        mp_drawing.draw_landmarks(
-            image=frame,
-            landmark_list=face_landmarks,
-            connections=mp_face_mesh.FACEMESH_IRISES,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp_drawing_styles
-            .get_default_face_mesh_iris_connections_style())
+            connection_drawing_spec=drawing_spec)
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow("Webcam", frame)
     
